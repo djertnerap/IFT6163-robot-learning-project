22728b71d1728486e21ac697c18bd8b96afe9ab1
Jétrémy Parent <djertnerap@live.ca>
2023-04-05 19:33:27 -0400

    Rat dataset generation script

--- walker.py
+++ walker.py
@@ -1,5 +1,10 @@
+import os.path
+
+import hydra
 import numpy as np
 import gymnasium as gym
+from PIL import Image
+from tqdm import tqdm
 
 from utils.trajectory import generate_traj
 
@@ -7,7 +12,20 @@ import environ
 
 
 def run_random_walk(time, seed):
-    env = gym.make('MiniWorld-OpenField-v1', view="agent", render_mode="human")
+    hydra.initialize(config_path="config", job_name="rat_random_walk_dataset_generator", version_base=None)
+    config = hydra.compose(config_name="config_test")
+    dataset_folder_path = config["hardware"]["dataset_folder_path"]
+
+    if not os.path.exists(dataset_folder_path):
+        os.mkdir(dataset_folder_path)
+
+    env = gym.make('MiniWorld-OpenField-v1',
+                   view="agent",
+                   render_mode="rgb_array",
+                   obs_width=64,
+                   obs_height=64,
+                   window_width=64,
+                   window_height=64)
     env.reset(seed=seed)
 
     pos = env.agent.pos
@@ -15,8 +33,9 @@ def run_random_walk(time, seed):
 
     ag, traj = generate_traj((np.array([pos[0], pos[2]]) - 0.5) / 10, -direction, time)
 
-    for t in range(traj.shape[1] - 1):
-        env.render()
+    for t in tqdm(range(traj.shape[1] - 1)):
+        # insert tqdm for image saving here
+        Image.fromarray(env.render()).save(os.path.join(dataset_folder_path, f"{t}.png"))
 
         # Slow movement speed to minimize resets
         action = traj[:, t + 1]

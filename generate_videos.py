5fa738d29cca70f2e78a502f8db19806fc8eaa1e
Alxec <darth.alxec@gmail.com>
2023-04-26 10:29:02 -0400

    Video generation

--- generate_videos.py
+++ generate_videos.py
@@ -5,10 +5,26 @@ from tqdm import tqdm
 from PIL import Image
 
 from rl import SACWithSpatialMemoryPipeline
+from vae import LitAutoEncoder
+
+
+#VAE
+checkpoint_path = os.path.abspath(os.getcwd()+"/checkpoints/rat_ae-epoch=23-train_loss=0.001255.ckpt")
+ae = LitAutoEncoder.load_from_checkpoint(
+    checkpoint_path, in_channels=3, net_config={
+                                    "output_channels": [16, 16, 32, 32],
+                                    "kernel_sizes": [5, 5, 3, 3],
+                                    "strides": [2, 2, 1, 1],
+                                    "paddings": [2, 2, 1, 1],
+                                    "output_paddings": [0, 0, 1, 1]  # for deconvolution
+                                        }.values()
+)
+ae.eval()
+
 
 # Variables
 nb_steps = 500
-checkpoint_path = ""
+checkpoint_path = os.path.abspath(os.getcwd()+"/checkpoints/last.ckpt")
 
 # Constants
 timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -24,7 +40,7 @@ if not os.path.exists(video_dir_path):
     os.makedirs(video_dir_path)
 
 # Model
-model = SACWithSpatialMemoryPipeline.load_from_checkpoint(checkpoint_path)
+model = SACWithSpatialMemoryPipeline.load_from_checkpoint(checkpoint_path, auto_encoder=ae).to('cuda')
 model.eval()
 
 # Loop to generate images
